239c15338e65731401317c2d0147791bf01487ba
nischalshakya15 <nischalshakya95@gmail.com>
2020-01-31 12:09:30 +0545

    Add 2nd sem analysis of dataset

--- work-related-learning/work-related-learning.R
+++ work-related-learning/work-related-learning.R
@@ -8,42 +8,82 @@ source('./utils.R')
 
 setwd('/home/nischal/nischalshakya15.github.io/work-related-learning')
 
-df <- read.csv('data-sets/2018/Jan/JavaProgrammingOne1stSem.csv')
+dfJan <- read.csv('data-sets/2018/Jan/JavaProgrammingOne1stSem.csv')
 
-colnames(df)
+colnames(dfJan)
 
 #Rename columns 
-df <- df %>% rename(Tutorial.Full.Marks = Full.Marks, Tutorial.Marks.Obtained = Marks.Obtained,
+dfJan <- dfJan %>% rename(Tutorial.Full.Marks = Full.Marks, Tutorial.Marks.Obtained = Marks.Obtained,
                     Group.Project.Full.Marks = Full.Marks.1, Group.Project.Obtained.Marks = Marks.Obtained.1,
                     Assignment.Full.Marks = Full.Marks.2, Assignment.Marks.Obtained = Marks.Obtained.2,
                     Mid.Term.Full.Marks = Full.Marks.3, Mid.Term.Marks.Obtained = Marks.Obtained.3, Mid.Term.FifteenPercent.Marks = Marks.Obtained.4,
                     Lab.Test.Full.Marks = Full.Marks.4, Lab.Test.Marks.Obtained = Marks.Obtained.5, Lab.Test.FifteenPercent.Marks = Marks.Obtained.6,
                     FinalExam.Full.Marks = Full.Marks.5, Final.Exam.Marks.Obtained = Marks.Obtained.7, Final.Exam.FortyPercent.Marks = Marks.Obtained..40.)
 
-colnames(df)
+colnames(dfJan)
 
 # Since Tutorials and Group Project are not used while calculating Internal marks we can exclude it from datasets
-df <- select(df, -c(Tutorial.Full.Marks, Tutorial.Marks.Obtained, Group.Project.Full.Marks, Group.Project.Obtained.Marks))
+dfJan <- select(dfJan, -c(Tutorial.Full.Marks, Tutorial.Marks.Obtained, Group.Project.Full.Marks, Group.Project.Obtained.Marks))
 
-uniqueGrades <- getUniqueAttribute(df$Grade)
+uniqueGrades <- getUniqueAttribute(dfJan$Grade)
 
-gradeDf <- data.frame()
+gradedfJan <- data.frame()
 
 for (u in uniqueGrades) {
-  gradeDf <- rbind(gradeDf, 
+  gradedfJan <- rbind(gradedfJan, 
                    data.frame(
-                     NoOfStudent = df %>% filter(Grade == u) %>% count(Grade)
+                     NoOfStudent = dfJan %>% filter(Grade == u) %>% count(Grade)
                    ))
 }
 
-gradeDf <- sortInAscendingOrder(gradeDf, gradeDf$NoOfStudent.n)
+gradedfJan <- sortInAscendingOrder(gradedfJan, gradedfJan$NoOfStudent.n)
 
-plotBarGraph(df = gradeDf, x = gradeDf$NoOfStudent.Grade, y = gradeDf$NoOfStudent.n, label = gradeDf$NoOfStudent.n, 
+plotBarGraph(df = gradedfJan, x = gradedfJan$NoOfStudent.Grade, y = gradedfJan$NoOfStudent.n, label = gradedfJan$NoOfStudent.n, 
              title = 'No of student on basics of grade',
              xlab = 'Grade', ylab = 'Total No of Student')
 
-topTenDf <- findTopTen(df, df$Grade.Point)
+topTendfJan <- findTopTen(dfJan, dfJan$Grade.Point)
 
-summary(df$Total.Marks)
+summary(dfJan$Total.Marks)
 
-sd(df$Total.Marks)
+sd(dfJan$Total.Marks)
+
+
+# 2nd semster student 
+dfJan2ndSem <- read.csv('data-sets/2018/Jan/JavaProgrammingOne2ndSem.csv')
+
+dfJan2ndSem <- dfJan2ndSem %>% rename(Tutorial.Full.Marks = Full.Marks, Tutorial.Marks.Obtained = Marks.Obtained,
+                                      Group.Project.Full.Marks = Full.Marks.1, Group.Project.Obtained.Marks = Marks.Obtained.1,
+                                      Assignment.Marks.Obtained = Marks.Obtained.2,
+                                      Mid.Term.Full.Marks = Full.Marks.2, Mid.Term.Marks.Obtained = Marks.Obtained.3, Mid.Term.FifteenPercent.Marks = Marks.Obtained.4,
+                                      Lab.Test.Full.Marks = Full.Marks.3, Lab.Test.Marks.Obtained = Marks.Obtained.5, Lab.Test.FifteenPercent.Marks = Marks.Obtained.6,
+                                      FinalExam.Full.Marks = Full.Marks.4, Final.Exam.Marks.Obtained = Marks.Obtained.7, Final.Exam.FortyPercent.Marks = Marks.Obtained..40.)
+
+colnames(dfJan2ndSem)
+
+dfJan2ndSem <- select(dfJan2ndSem, -c(Tutorial.Full.Marks, Tutorial.Marks.Obtained, Group.Project.Full.Marks, Group.Project.Obtained.Marks))
+
+uniqueGradeJan2ndSem <- getUniqueAttribute(dfJan2ndSem$Grade)
+
+print(uniqueGradeJan2ndSem)
+
+gradedfJan2ndSem <- data.frame()
+
+for (u in uniqueGradeJan2ndSem) {
+  gradedfJan2ndSem <- rbind(gradedfJan2ndSem, 
+                      data.frame(
+                        NoOfStudent = dfJan2ndSem %>% filter(Grade == u) %>% count(Grade)
+                      ))
+}
+
+gradedfJan2ndSem <- sortInAscendingOrder(gradedfJan2ndSem, gradedfJan2ndSem$NoOfStudent.n)
+
+plotBarGraph(df = gradedfJan2ndSem, x = gradedfJan2ndSem$NoOfStudent.Grade, y = gradedfJan2ndSem$NoOfStudent.n, label = gradedfJan2ndSem$NoOfStudent.n, 
+             title = 'No of student on basics of grade',
+             xlab = 'Grade', ylab = 'Total No of Student')
+
+topTendfJan <- findTopTen(dfJan2ndSem, dfJan2ndSem$Grade.Point)
+
+summary(dfJan2ndSem$Total)
+
+sd(dfJan2ndSem$Total)
