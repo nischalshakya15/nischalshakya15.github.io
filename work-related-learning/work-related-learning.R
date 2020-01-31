e751c52893506154e28e166356a40e317163e95a
nischalshakya15 <nischalshakya95@gmail.com>
2020-01-31 23:42:04 +0545

    Remove unwanted datasets from folder

--- work-related-learning/work-related-learning.R
+++ work-related-learning/work-related-learning.R
@@ -8,86 +8,180 @@ source('./utils.R')
 
 setwd('/home/nischal/nischalshakya15.github.io/work-related-learning')
 
-dfJan <- read.csv('data-sets/2018/Jan/JavaProgrammingOne1stSem.csv')
+# R code for Java Programming One
+javaProgrammingOneDf <- read.csv('data-sets/JavaProgrammingOne.csv')
 
-colnames(dfJan)
+colnames(javaProgrammingOneDf)
 
-#Rename columns 
-dfJan <- dfJan %>% rename(Tutorial.Full.Marks = Full.Marks, Tutorial.Marks.Obtained = Marks.Obtained,
-                    Group.Project.Full.Marks = Full.Marks.1, Group.Project.Obtained.Marks = Marks.Obtained.1,
-                    Assignment.Full.Marks = Full.Marks.2, Assignment.Marks.Obtained = Marks.Obtained.2,
-                    Mid.Term.Full.Marks = Full.Marks.3, Mid.Term.Marks.Obtained = Marks.Obtained.3, Mid.Term.FifteenPercent.Marks = Marks.Obtained.4,
-                    Lab.Test.Full.Marks = Full.Marks.4, Lab.Test.Marks.Obtained = Marks.Obtained.5, Lab.Test.FifteenPercent.Marks = Marks.Obtained.6,
-                    FinalExam.Full.Marks = Full.Marks.5, Final.Exam.Marks.Obtained = Marks.Obtained.7, Final.Exam.FortyPercent.Marks = Marks.Obtained..40.)
+# Give meaning ful row names
+javaProgrammingOneDf <- javaProgrammingOneDf %>% rename(
+                        Tutorial.Full.Marks = Full.Marks, Tutorial.Marks.Obtained = Marks.Obtained,
+                        Group.Project.Full.Marks = Full.Marks.1, Group.Project.Obtained.Marks = Marks.Obtained.1,
+                        Assignment.Full.Marks = Full.Marks.2, Assignment.Marks.Obtained = Marks.Obtained.2,
+                        Mid.Term.Full.Marks = Full.Marks.3, Mid.Term.Marks.Obtained = Marks.Obtained.3, Mid.Term.FifteenPercent.Marks = Marks.Obtained.4,
+                        Lab.Test.Full.Marks = Full.Marks.4, Lab.Test.Marks.Obtained = Marks.Obtained.5, Lab.Test.FifteenPercent.Marks = Marks.Obtained.6,
+                        FinalExam.Full.Marks = Full.Marks.5, Final.Exam.Marks.Obtained = Marks.Obtained.7, Final.Exam.FortyPercent.Marks = Marks.Obtained..40.)
 
-colnames(dfJan)
+colnames(javaProgrammingOneDf)
 
-# Since Tutorials and Group Project are not used while calculating Internal marks we can exclude it from datasets
-dfJan <- select(dfJan, -c(Tutorial.Full.Marks, Tutorial.Marks.Obtained, Group.Project.Full.Marks, Group.Project.Obtained.Marks))
+# Since, tutorial marks and group project marks is not used for final calculation of internal marks we can drop it from dataframe 
+javaProgrammingOneDf <- select(javaProgrammingOneDf, -c(Tutorial.Full.Marks, Tutorial.Marks.Obtained, Group.Project.Full.Marks, Group.Project.Obtained.Marks))
 
-uniqueGrades <- getUniqueAttribute(dfJan$Grade)
+# filter unique grade
+uniqueGradeJavaProgrammingOne <- getUniqueAttribute(javaProgrammingOneDf$Grade)
 
-gradedfJan <- data.frame()
+print(uniqueGradeJavaProgrammingOne)
 
-for (u in uniqueGrades) {
-  gradedfJan <- rbind(gradedfJan, 
-                   data.frame(
-                     NoOfStudent = dfJan %>% filter(Grade == u) %>% count(Grade)
-                   ))
-}
+javaProgrammingOneCountDf <- data.frame()
 
-gradedfJan <- sortInAscendingOrder(gradedfJan, gradedfJan$NoOfStudent.n)
+str(javaProgrammingOneDf)
 
-plotBarGraph(df = gradedfJan, x = gradedfJan$NoOfStudent.Grade, y = gradedfJan$NoOfStudent.n, label = gradedfJan$NoOfStudent.n, 
-             title = 'No of student on basics of grade',
-             xlab = 'Grade', ylab = 'Total No of Student')
-
-topTendfJan <- findTopTen(dfJan, dfJan$Grade.Point)
-
-summary(dfJan$Total.Marks)
-
-sd(dfJan$Total.Marks)
-
-
-# 2nd semster student 
-dfJan2ndSem <- read.csv('data-sets/2018/Jan/JavaProgrammingOne2ndSem.csv')
-
-dfJan2ndSem <- dfJan2ndSem %>% rename(Tutorial.Full.Marks = Full.Marks, Tutorial.Marks.Obtained = Marks.Obtained,
-                                      Group.Project.Full.Marks = Full.Marks.1, Group.Project.Obtained.Marks = Marks.Obtained.1,
-                                      Assignment.Marks.Obtained = Marks.Obtained.2,
-                                      Mid.Term.Full.Marks = Full.Marks.2, Mid.Term.Marks.Obtained = Marks.Obtained.3, Mid.Term.FifteenPercent.Marks = Marks.Obtained.4,
-                                      Lab.Test.Full.Marks = Full.Marks.3, Lab.Test.Marks.Obtained = Marks.Obtained.5, Lab.Test.FifteenPercent.Marks = Marks.Obtained.6,
-                                      FinalExam.Full.Marks = Full.Marks.4, Final.Exam.Marks.Obtained = Marks.Obtained.7, Final.Exam.FortyPercent.Marks = Marks.Obtained..40.)
-
-colnames(dfJan2ndSem)
-
-dfJan2ndSem <- select(dfJan2ndSem, -c(Tutorial.Full.Marks, Tutorial.Marks.Obtained, Group.Project.Full.Marks, Group.Project.Obtained.Marks))
-
-uniqueGradeJan2ndSem <- getUniqueAttribute(dfJan2ndSem$Grade)
-
-print(uniqueGradeJan2ndSem)
-
-gradedfJan2ndSem <- data.frame()
-
-for (u in uniqueGradeJan2ndSem) {
-  gradedfJan2ndSem <- rbind(gradedfJan2ndSem, 
+for (u in uniqueGradeJavaProgrammingOne) {
+  javaProgrammingOneCountDf <- rbind(javaProgrammingOneCountDf, 
                       data.frame(
-                        NoOfStudent = dfJan2ndSem %>% filter(Grade == u) %>% count(Grade)
+                        NoOfStudent = javaProgrammingOneDf %>% filter(Grade == u) %>% count(Grade)
                       ))
 }
 
-gradedfJan2ndSem <- sortInAscendingOrder(gradedfJan2ndSem, gradedfJan2ndSem$NoOfStudent.n)
+#Sort the count in ascending order on basics of grade 
+javaProgrammingOneCountDf <- sortInAscendingOrder(javaProgrammingOneCountDf, javaProgrammingOneCountDf$NoOfStudent.n)
 
-plotBarGraph(df = gradedfJan2ndSem, x = gradedfJan2ndSem$NoOfStudent.Grade, y = gradedfJan2ndSem$NoOfStudent.n, label = gradedfJan2ndSem$NoOfStudent.n, 
+plotBarGraph(df = javaProgrammingOneCountDf, x = javaProgrammingOneCountDf$NoOfStudent.Grade, 
+             y = javaProgrammingOneCountDf$NoOfStudent.n, label = javaProgrammingOneCountDf$NoOfStudent.n, 
              title = 'No of student on basics of grade',
              xlab = 'Grade', ylab = 'Total No of Student')
 
-topTendfJan <- findTopTen(dfJan2ndSem, dfJan2ndSem$Grade.Point)
+# find top 5 student
+javaProgrammingOneTopFive <- findTopTen(javaProgrammingOneDf, javaProgrammingOneDf$Grade.Point, top = 5)
 
-summary(dfJan2ndSem$Total)
+# Sort on basics of grade point
+javaProgrammingOneTopFive <- sortInAscendingOrder(javaProgrammingOneTopFive, javaProgrammingOneTopFive$Grade.Point)
 
-sd(dfJan2ndSem$Total)
+# Total number of student attending exam
+totalStudentInJavaOne <- javaProgrammingOneCountDf %>% select(NoOfStudent.n) %>% sum()
 
-print(dfJan$Name)
+print(totalStudentInJavaOne)
 
-jandf <- data.frame(Name = c(dfJan$Name))
+# Genearate summary 
+summary(javaProgrammingOneDf$Total)
+
+# Find out standard deviation 
+sd(javaProgrammingOneDf$Total)
+# R code for java programming one ends 
+
+# R code for java programming two 
+javaTwoDf <- read.csv('data-sets/JavaProgrammingTwo.csv')
+
+colnames(javaTwoDf)
+
+javaTwoDf <- javaTwoDf %>% rename(
+             Mid.Term.Full.Marks = Full.Marks, Mid.Term.Marks.Obtained = Score, Mid.Term.Fifteen.Percent.Marks = Marks.Obtained..15..,
+             Lab.Test.Full.Marks = Full.Marks.1, Lab.Test.Marks.Obtained = Marks.Obtained..15...1,
+             Assignment.Full.Marks = Full.Marks.2, Assignment.Marks.Obtained = Marks.Obtained..25..,
+             Tutorial.Full.Marks = Full.Marks.3, Tutorial.Marks.Obtained = Marks.Obtained..5..,
+             Internal.Marks = Internal.Marks..60.., 
+             Final.Exam.Full.Marks = Full.Marks.4, Final.Exam.Marks.Obtained = Score.1, Final.Exam.FortyPercent.Marks = Marks.Obtained..40..,
+             Total = Total..100.., 
+             Grade = Grade.after.40..rule
+)
+
+colnames(javaTwoDf)
+
+# filter unique grade
+uniqueGradeJavaTwo <- getUniqueAttribute(javaTwoDf$Grade)
+
+print(uniqueGradeJavaTwo)
+
+javaTwoCountDf <- data.frame()
+
+str(javaTwoDf)
+
+for (u in uniqueGradeJavaTwo) {
+  javaTwoCountDf <- rbind(javaTwoCountDf, 
+                                     data.frame(
+                                       NoOfStudent = javaTwoDf %>% filter(Grade == u) %>% count(Grade)
+                                     ))
+}
+
+#Sort the count in ascending order on basics of grade 
+javaTwoCountDf <- sortInAscendingOrder(javaTwoCountDf, javaTwoCountDf$NoOfStudent.n)
+
+plotBarGraph(df = javaTwoCountDf, x = javaTwoCountDf$NoOfStudent.Grade, 
+             y = javaTwoCountDf$NoOfStudent.n, label = javaTwoCountDf$NoOfStudent.n, 
+             title = 'No of student on basics of grade',
+             xlab = 'Grade', ylab = 'Total No of Student')
+
+# find top 5 student
+javaTwoTopFive <- findTopTen(javaTwoDf, javaTwoDf$Grade.Point, top = 5)
+
+# Sort on basics of grade point
+javaTwoTopFive <- sortInAscendingOrder(javaTwoTopFive, javaTwoTopFive$Grade.Point)
+
+# Total number of student attending exam
+totalStudentInJavaTwo <- javaTwoCountDf %>% select(NoOfStudent.n) %>% sum()
+
+print(totalStudentInJavaTwo)
+
+# Genearate summary 
+summary(javaTwoDf$Total)
+
+# Find out standard deviation 
+sd(javaTwoDf$Total)
+
+# load csv file into data frame
+javaWebDf <- read.csv('data-sets/JavaWebProgramming.csv')
+
+colnames(javaWebDf)
+
+javaWebDf <- javaWebDf %>% rename(
+             Mid.Term.Full.Marks = Full.Marks, Mid.Term.Marks.Obtained = Score, Mid.Term.Twenty.Percent.Marks = Marks.Obtained..20..,
+             Individual.Assignment.Full.Marks = Full.Marks.1, Individual.Assignment.Marks.Obtained = Assignment.Score..20.,  
+             Group.Assignment.Full.Marks = Full.Marks.2, Group.Assignment.Marks.Obtained = Group.Proj..Score..20.,
+             Internal.Marks = Internal.Marks..60..,
+             Final.Exam.Full.Marks = Full.Marks.3, Final.Exam.Marks.Obtained = Score.1, Final.Exam.FortyPercent.Marks=Marks.Obtained..40..,
+             Total = Total..100.., 
+             Grade = Grade.after.40..rule
+)
+
+colnames(javaWebDf)
+
+# filter unique grade
+uniqueGradeJavaWeb <- getUniqueAttribute(javaWebDf$Grade)
+
+print(uniqueGradeJavaWeb)
+
+javaWebCountDf <- data.frame()
+
+str(javaWebDf)
+
+for (u in uniqueGradeJavaWeb) {
+  javaWebCountDf <- rbind(javaWebCountDf, 
+                          data.frame(
+                            NoOfStudent = javaWebDf %>% filter(Grade == u) %>% count(Grade)
+                          ))
+}
+
+#Sort the count in ascending order on basics of grade 
+javaWebCountDf <- sortInAscendingOrder(javaWebCountDf, javaWebCountDf$NoOfStudent.n)
+
+plotBarGraph(df = javaWebCountDf, x = javaWebCountDf$NoOfStudent.Grade, 
+             y = javaWebCountDf$NoOfStudent.n, label = javaWebCountDf$NoOfStudent.n, 
+             title = 'No of student on basics of grade',
+             xlab = 'Grade', ylab = 'Total No of Student')
+
+# find top 5 student
+javaWebTopFive <- findTopTen(javaWebDf, javaWebDf$Grade.Point, top = 5)
+
+# Sort on basics of grade point
+javaWebTopFive <- sortInAscendingOrder(javaWebTopFive, javaWebTopFive$Grade.Point)
+
+# Total number of student attending exam
+totalStudentInJavaWeb <- javaWebCountDf %>% select(NoOfStudent.n) %>% sum()
+
+print(totalStudentInJavaWeb)
+
+# Genearate summary 
+summary(javaWebDf$Total)
+
+# Find out standard deviation 
+sd(javaWebDf$Total)
