f37fc0c66a725bc05b4b02ba99066071acfe9c9d
nischalshakya95 <nischalshakya@lftechnology.com>
2021-05-24 21:46:30 +0545

    Filter global_sales which is not equal to zero

--- thesis/src/predective-model.R
+++ thesis/src/predective-model.R
@@ -11,23 +11,18 @@ setwd('/home/nischal/repository/personal/nischalshakya15.github.io/thesis')
 # Read data from csv file
 data <- read.csv('data-sets/vgsales-12-4-2019.csv', stringsAsFactors = FALSE)
 
-# Select only the required columns
 df <- data %>%
-  select(Name, Genre, Platform, Publisher, Developer, Total_Shipped,
-         Global_Sales, NA_Sales, PAL_Sales, JP_Sales, Other_Sales, Year) %>%
-  filter(data$Year %in% (2009:2017) & (data$Platform == 'PC' |
-    data$Platform == 'PS4' |
-    data$Platform == 'PS3' |
-    data$Platform == 'PS2' |
-    data$Platform == 'XOne' |
-    data$Platform == 'XB')) %>%
-  arrange(Year)
-# Select the ranege from 2009 to 2014
-df_2009_to_2014 <- df %>%
-  filter(df$Year %in% (2009:2014))
-# Select the range from 2015 to 2018
-df_2015_to_2018 <- df %>%
-  filter(df$Year %in% (2015:2017)) %>%
+  select(Name, Genre, Platform, Publisher, Developer,
+         Global_Sales, NA_Sales, JP_Sales, PAL_Sales, Other_Sales, Year) %>%
+  filter(
+    data$Year %in% (2000:2020) &
+      !is.na(data$Global_Sales) & data$Global_Sales != 0.00 &
+      (data$Platform == 'PC' |
+        data$Platform == 'PS2' |
+        data$Platform == 'PS3' |
+        data$Platform == 'PS4' |
+        data$Platform == 'XOne' |
+        data$Platform == 'XB')) %>%
   arrange(Year)
 
 # Define UI
@@ -35,7 +30,7 @@ ui <- fluidPage(theme = shinytheme('lumen'),
                 titlePanel('Predictive Model using R'),
                 fluidRow(
                   column(3, align = 'left', offset = 1, selectInput(inputId = 'type', label = strong('Select Year'),
-                                                                    choices = unique(df$Year), selected = '2009')
+                                                                    choices = unique(df$Year), selected = '2001')
                   ),
                   column(3, selectInput(inputId = 'genre', label = strong('Select Genre'),
                                         choices = unique(df$Genre), selected = 'Action')
