b6fe20985e86366d38f03632801b7d82213dc5d3
nischalshakya95 <nischalshakya@lftechnology.com>
2021-05-27 10:11:39 +0545

    Update functional Code

--- thesis/src/predictive-model.R
+++ thesis/src/predictive-model.R
@@ -7,23 +7,12 @@ library('shinythemes')
 library('reader')
 library('tidyr')
 
+# Set the working directory
 setwd('/home/nischal/repository/personal/nischalshakya15.github.io/thesis')
 
-source('utils/utils.R')
-
-# Set the working directory
+source('src/utils/utils.R')
 
 # Read data from csv file
 data <- read.csv(file = 'data-sets/vgsales-processed.csv', sep = ',', dec = '.', stringsAsFactors = FALSE)
-
-find_by_column_name <- function(df, col_name, col_value, arrange_col_name) {
-  return(
-    df
-      %>%
-      filter(df[col_name] == col_value)
-      %>%
-      arrange(arrange_col_name)
-  )
-}
 
 df <- find_by_column_name(data, 'Platform', 'PS2', 'Year')
--- thesis/src/utils/utils.R
+++ thesis/src/utils/utils.R
@@ -1,6 +1,9 @@
-find_by_column_name <- function(df, col_name, col_value) {
+find_by_column_name <- function(df, col_name, col_value, arrange_col_name) {
   return(
-    df %>%
+    df
+      %>%
       filter(df[col_name] == col_value)
+      %>%
+      arrange(arrange_col_name)
   )
 }
