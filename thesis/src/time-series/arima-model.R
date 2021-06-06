366c3c0b9b3ce02ecd86da628d3a9553892c366a
nischalshakya95 <nischalshakya@lftechnology.com>
2021-06-06 14:45:32 +0545

    Completed ARIMA model

--- thesis/src/time-series/arima-model.R
+++ thesis/src/time-series/arima-model.R
@@ -11,13 +11,12 @@ source('src/utils/utils.R')
 df <- read.csv(file = 'data-sets/pc-sales.csv', sep = ',', dec = '.')
 df_ts <- ts(df$Sales, start = 2000, frequency = 12)
 
-a1 <- auto.arima(df_ts, trace = TRUE, ic = 'aic', approximation = FALSE)
-
 df_model <- window(x = df_ts, start = 2000, end = 2010)
 df_test <- window(x = df_ts, start = 2011)
 
-fit <- arima(df_model, order = c(1, 0, 0))
-forecastedValues <- forecast(fit, 60)
+a2 <- auto.arima(df_model, D = 1, trace = TRUE, ic = 'aic', approximation = FALSE)
+forecastedValues <- forecast(a2, 60)
+print(forecastedValues)
 
 df_arima_forecast <- cbind("Year" = rownames(as.data.frame(forecastedValues)), as.data.frame(forecastedValues))
 names(df_arima_forecast) <- gsub(" ", "_", names(df_arima_forecast))  # Removing whitespace from column names
